3fd438cdf9a4ef11c696f267fa404763434a70c4
linluxx <karolina.kkv@gmail.com>
2022-10-22 17:06:19 +0300

    added localstorage

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    accent: '#dafaff',
+    dark: '#3a3a3a',
+    light: '#ffffff',
+  },
+  font: {
+    main: 'Courier New, Courier, monospace',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
